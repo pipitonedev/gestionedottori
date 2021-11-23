0239dd35693a1b4e61c2be0e10bc3bfba30c837b
Vincenzo Pipitone <82387646+pipitonedev@users.noreply.github.com>
2021-11-23 10:35:54 +0100

    Creazione Repository

---
+++ src/main/java/it/prova/gestionedottori/repository/DottoreRepository.java
@@ -0,0 +1,9 @@
+package it.prova.gestionedottori.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestionedottori.model.Dottore;
+
+public interface DottoreRepository extends CrudRepository<Dottore, Long> {
+
+}
